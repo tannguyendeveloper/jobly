cfc86606d1e2c8167535959b828c38f1894a320a
Tan Nguyen <tan@tannguyendeveloper.com>
2021-05-09 23:06:24 -0700

    fix redirect after signing up

--- src/components/App/pages/SignUpPage/SignUpPage.js
+++ src/components/App/pages/SignUpPage/SignUpPage.js
@@ -1,8 +1,12 @@
+import { useContext } from 'react';
+import AppContext from '../../../../contexts/AppContext';
 import SignUpForm from '../../SignUpForm';
+import { Redirect } from 'react-router-dom';
+
 const SignUpPage = () => {
-  return (<>
-    <SignUpForm />
-  </>)
+  const AppState = useContext(AppContext);
+  console.log(AppState);
+  return !AppState?.user?.isLoggedIn ? <SignUpForm /> : <Redirect to="/" />
 }
 
 export default SignUpPage;
